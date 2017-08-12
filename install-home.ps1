620b971be4d768acf652face1112b31d4b582b73
Ruslan Gunawardana <mr.exarus@gmail.com>
2017-08-12 17:27:45 +0300

    Updated home and win10 applications installation instructions

--- install-home.ps1
+++ install-home.ps1
@@ -3,3 +3,5 @@ sudo choco install steam -y
 
 # Download manually:
 # - Blizzard App
+# - Twitch
+# - OBS Studio
--- install-win10.ps1
+++ install-win10.ps1
@@ -5,15 +5,31 @@ Enable-WindowsOptionalFeature -Online -FeatureName:Microsoft-Hyper-V -All
 # - Docker for Windows
 
 # Download from Store
-# - Google
 # - Twitter
+# - Facebook
+# - Instagram
 # - Skype
 # - Slack
-# - Facebook
 # - Messenger
 # - Viber
-# - Instagram
 # - Telegram Desktop
-# - DjVu Reader
+#
+# - Scan
+# - Djvu Reader
+# - QR Code for Windows 10
+# - Focus 10
+# - Duolingo
+# - SHARE.it
+# - Evernote
+# - OneNote
+# - Uber
+# - Dropbox
+# - Wikipedia
+#
 # - uBlock Origin for Microsoft Edge
 # - Translator for Microsoft Edge
+# - Office for Microsoft Edge
+
+# Remove:
+# - Paid Wi-Fi & Cellular
+# - Get Office
